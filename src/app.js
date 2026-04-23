fc5fcdb1a2527f6d8f3fafce458725cc242dceb6
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-04-23 17:52:45 +0530

    style: format code for consistency and readability

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get("/",(req,res) =>{
-    res.status(200).send("Hello from Acquistions!")
-})
+app.get('/', (req, res) => {
+  res.status(200).send('Hello from Acquistions!');
+});
 
 export default app;
