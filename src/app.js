d8848fdb645ca5eee78eeb1bc3c526d047a77341
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-05-12 21:38:23 +0530

    feat: add security middleware to enhance application security

--- src/app.js
+++ src/app.js
@@ -5,11 +5,13 @@ import morgan from 'morgan';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRouter from '#routes/auth.route.js';
+import securityMiddleware from '#middleware/security.middleware.js';
 
 
 const app = express();
 
 app.use(helmet());
+app.use(securityMiddleware);
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
