7c785b3288f75352174da09a0e64fbd2ac51b69b
Shakthivelk24 <shakthivelk1124@gmail.com>
2026-05-14 16:08:14 +0530

    feat: bind server to all network interfaces for accessibility

--- src/server.js
+++ src/server.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Server is running');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
